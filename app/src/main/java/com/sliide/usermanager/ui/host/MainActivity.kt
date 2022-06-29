22c1444e07e34d8d12404005e3d8e7549e7f5fa0
Viorel Albastrelu <albastrelu.viorel@gmail.com>
2022-06-29 22:48:37 +0300

    UI improvements on UserList screen

--- app/src/main/java/com/sliide/usermanager/ui/host/MainActivity.kt
+++ app/src/main/java/com/sliide/usermanager/ui/host/MainActivity.kt
@@ -2,6 +2,11 @@ package com.sliide.usermanager.ui.host
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.sliide.usermanager.R
 import com.sliide.usermanager.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,10 +14,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.nav_main))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
